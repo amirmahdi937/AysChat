99069af5f0cff5772e02a1a701bdfe9c2d0881af
Amir <AysChat.com>
2025-07-09 21:41:59 +0430

    AysChat

--- AysChatProject/settings.py
+++ AysChatProject/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'AysChatProgect.urls'
+ROOT_URLCONF = 'AysChatProject.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'AysChatProgect.wsgi.application'
+WSGI_APPLICATION = 'AysChatProject.wsgi.application'
 
 
 # Database
