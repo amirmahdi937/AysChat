d023fdc38c783bc33d99ce06afac35ffd4ce9eb1
AmirmahdiAzizi <158186442+amirmahdi937@users.noreply.github.com>
2025-07-13 20:20:17 +0330

    Update settings.py

--- AysChatProject/settings.py
+++ AysChatProject/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-GEMINI_API_KEY = os.getenv('ayschat.onrender.com')
+GEMINI_API_KEY = os.getenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
