1d7689bbdc1d78c1afaf36b3140b9f9554efe717
AmirmahdiAzizi <158186442+amirmahdi937@users.noreply.github.com>
2025-07-13 21:25:26 +0330

    Update urls.py

--- chatbot_app/urls.py
+++ chatbot_app/urls.py
@@ -1,7 +1,9 @@
+# AysChat_Django/chat_app/urls.py
+
 from django.urls import path
-from . import views
+from . import views # از .views برای ایمپورت کردن views از همین اپلیکیشن استفاده کنید
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chat/', include('chat_app.urls')),
+    path('', views.chat_api, name='chat_api'), # این خط اصلی API شماست. نام 'chat_api' را به آن بدهید.
+    # !!! تمام خطوط دیگر (مثل path('admin/', ...) یا path('chat/', include(...))) را حذف کنید !!!
 ]
