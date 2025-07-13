eacdd7c67131e1f84e27cb4936a944a7902fe141
AmirmahdiAzizi <158186442+amirmahdi937@users.noreply.github.com>
2025-07-13 20:28:35 +0330

    Update settings.py

--- AysChatProject/settings.py
+++ AysChatProject/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-GEMINI_API_KEY = os.getenv('.env')
+GEMINI_API_KEY = os.getenv('ayschat.onrender.com')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
